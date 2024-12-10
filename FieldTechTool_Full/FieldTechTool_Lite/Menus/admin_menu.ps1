95e6886ff8aedda497056dd8b2f620ecc2b5d93a
Koupa <ktayl21@yahoo.com.au>
2024-12-10 15:13:29 +1000

    Create Basic GPU Driver Menu Option
    Added basic view of available GPU Drivers and their respective download URLs

--- FieldTechTool_Full/FieldTechTool_Lite/Menus/admin_menu.ps1
+++ FieldTechTool_Full/FieldTechTool_Lite/Menus/admin_menu.ps1
@@ -4,6 +4,7 @@
 . .\Scripts\Administration\DPLink_HPIA_Final_Updates.ps1
 . .\Scripts\Administration\robocopy.ps1
 . .\Scripts\Administration\840_G9_add.ps1
+. .\Scripts\Administration\GPU_driver_detection.ps1
 function Show-AdminMenu {
     $toolRootDirectory = Resolve-Path .
     do {
@@ -11,6 +12,7 @@ function Show-AdminMenu {
         Write-Host '2. Backup user data with Robocopy'
         Write-Host '3. Disable Sleep and Hibernation on this machine'
         Write-Host "4. Jack's script for 840 G9 (Intel) Autopilot join"
+        Write-Host "5. Check for GPU Driver Update"
         Write-Host 'q. Previous menu'
         $choice = Read-Host 'Please choose an option'
         switch ($choice.ToLower()) {
@@ -36,6 +38,34 @@ function Show-AdminMenu {
                 Start-Sleep -Seconds 1.5
                 Get-JackInfo
                 Clear-Host
+             }
+             '5' {
+                Write-Host "Triggered GPU Driver Update Check" -ForegroundColor Green
+                Start-Sleep -Seconds 1.5
+                Clear-Host
+
+                # Find Drivers, enable progress output so user doesn't think everything broke
+                $GPUDrivers = FindRelevantGPUDrivers -OutputProgress
+
+                if ($GPUDrivers.Count -eq 0) {
+                    Write-Host "Couldn't find any Drivers online, this may be an error!"
+                } else {
+                    Write-Host "Found $($GPUDrivers.Count) potential driver(s)..." -ForegroundColor Gray
+
+                    $Device = GetDriverDeviceInformation
+                    Write-Host "`nCurrent GPU Driver Version: $($Device.ShortGPUDriverVersion)" -ForegroundColor Yellow
+
+                    Write-Host "`nDrivers Available:"
+                    foreach ($Driver in $GPUDrivers) {
+                        Write-Host "$($Driver.ProductName)" -ForegroundColor Yellow
+                        Write-Host "`t$($Driver.DriverName) [Version: $($Driver.DriverVersion)]"
+                        Write-Host "`tDownload URL [$($Driver.DownloadSize)]: $($Driver.DownloadURL)"
+                    }
+                }
+
+                Read-Host "`nPress enter to continue...";
+                Clear-Host
+
              }
              'q' {
                 Clear-Host
