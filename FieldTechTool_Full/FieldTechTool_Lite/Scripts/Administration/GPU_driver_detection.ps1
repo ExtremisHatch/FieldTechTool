0c00f2e1d88d4d38eb86066450a0b2202bed4e2a
Koupa <ktayl21@yahoo.com.au>
2024-12-12 13:50:16 +1000

    Move Menu Selections
    Move GPU Driver menu selections to GPU_driver_detection sub-script

--- FieldTechTool_Full/FieldTechTool_Lite/Scripts/Administration/GPU_driver_detection.ps1
+++ FieldTechTool_Full/FieldTechTool_Lite/Scripts/Administration/GPU_driver_detection.ps1
@@ -198,3 +198,79 @@ function FindRelevantGPUDrivers {
     return $GPUDriverResults
 }
 
+function ProvideUserGPUDriverOptions {
+    # Find Drivers, enable progress output so user doesn't think everything broke
+    $GPUDrivers = FindRelevantGPUDrivers -OutputProgress
+    
+    if ($GPUDrivers.Count -eq 0) {
+        Write-Host "Couldn't find any Drivers online, this may be an error!"
+    } else {
+        Write-Host "Found $($GPUDrivers.Count) potential driver(s)..." -ForegroundColor Gray
+    
+        $Device = GetDriverDeviceInformation
+    
+        # Current GPU Driver Version (Shortened NVidia "Standard" lol)
+        $CurrentVersion = $Device.ShortGPUDriverVersion | % { @{Version=([double]$_);Major=([int]$_.Split('.')[0]);Minor=([int]$_.Split('.')[1])} }
+        
+        # Get the latest available version out of the results, then manipulate into a nice convenient object (Very clean, totally doesn't need a rewrite)
+        $LatestAvailable = ($GPUDrivers.DriverVersion | %{[double]$_} | Sort-Object -Descending | Select-Object -First 1) | `
+                            % { @{Version=$_;Major=([int]$_.ToString().Split('.')[0]);Minor=([int]$_.ToString().Split('.')[1])} }
+        
+        $Outdated = $LatestAvailable.Version -gt $CurrentVersion.Version
+        $OutdatedType = if ($Outdated) { if ($LatestAvailable.Major -gt $CurrentVersion.Major) {"Major"} else {"Minor"} } else { $null }
+    
+        # List out current details for user
+        Write-Host "`nCurrent GPU: $($Device.GPUName)" -ForegroundColor Yellow
+    
+        # If Outdated, don't print new line yet
+        Write-Host "`tDriver Version: $($Device.ShortGPUDriverVersion)" -ForegroundColor Yellow -NoNewline:$Outdated 
+    
+        if ($Outdated) {
+            Write-Host " [Potential $OutdatedType Update Available]" -ForegroundColor Magenta
+        }
+    
+        Write-Host "`nDrivers Available:"
+        for ($i=0; $i -lt $GPUDrivers.Count; $i++) {
+            $Driver = $GPUDrivers[$i]
+            Write-Host "[$($i+1)] $($Driver.ProductName)" -ForegroundColor Yellow
+            Write-Host "`t$($Driver.DriverName) [Version: $($Driver.DriverVersion)]"
+            Write-Host "`tDownload URL [$($Driver.DownloadSize)]: $($Driver.DownloadURL)"
+        }
+    
+        $DownloadDriver = @("Y","YES") -contains (Read-Host "`nWould you like to download a listed GPU Driver? (Y/n)")
+        if ($DownloadDriver) {
+            while (($DriverIndex = Read-Host "Please input the Number of the GPU Driver you wish to download") -ne $null) {
+                # Ensure it's a number
+                if ($DriverIndex -notmatch "^(\d)*$") { continue; }
+                
+                # Cast to Int
+                $DriverIndex = [int]$DriverIndex;
+    
+                # If it's within range, break!
+                if ($DriverIndex -le $GPUDrivers.Count -and $DriverIndex -gt 0) { break; }
+            }
+    
+            # Driver Selected by User
+            $SelectedDriver = $GPUDrivers[$DriverIndex-1]
+            $DownloadURL = $SelectedDriver.DownloadURL
+    
+            # If DownloadURL ends with ".exe", ".app", ".XXX", we will keep the end of the URL as the file name
+            $FileName = if ($DownloadURL -match "\.[a-zA-Z0-9]{2,3}$") { $DownloadURL.Substring($DownloadURL.LastIndexOf('/') + 1) } else {'GPU-Driver.exe'}
+            
+            # Local Output Path
+            $OutputPath = ".\Drivers\"
+            $DriverLocation = "$($OutputPath)$($FileName)"
+            if (-not (Test-Path $OutputPath)) {
+                New-Item -Path $OutputPath -ItemType "Directory" > $null
+            }
+    
+            Write-Host "Downloading `"$($SelectedDriver.ProductName)`" driver [$($SelectedDriver.DownloadSize)]"
+            Write-Host "`tThis may take some time..." -ForegroundColor Gray
+    
+            Invoke-WebRequest -Uri $DownloadURL -OutFile $DriverLocation
+    
+            Write-Host "Download complete!" -ForegroundColor Green
+            Write-Host "Driver Location: $((Get-Item $DriverLocation).FullName)"
+        }
+    }
+}
