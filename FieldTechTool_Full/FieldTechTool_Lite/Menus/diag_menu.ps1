16c38952b9b9a06b579a70545f0373b4c10fc07a
Koupa <ktayl21@yahoo.com.au>
2025-01-08 09:47:43 +1000

    Update Diag Menu

--- FieldTechTool_Full/FieldTechTool_Lite/Menus/diag_menu.ps1
+++ FieldTechTool_Full/FieldTechTool_Lite/Menus/diag_menu.ps1
@@ -7,104 +7,92 @@
 . .\Scripts\Diagnostics\user_usage.ps1
 
 function Show-DiagMenu {
-    do {
-        Write-Host '1. Gather summary info like PC name, IP, OS, etc.'
-        Write-Host '2. Run a user simulation on the system **Output in Results folder**'
-        Write-Host '3. Gather system logs. **Output in Results folder**'
-        Write-Host '4. Query Users Last Desktop Usage'
-        Write-Host '5. Check Drives for usage in the last 180 days'
-        Write-Host 'q. Previous menu'
-        $choice = Read-Host 'Please choose an option'
-        switch ($choice.ToLower()) {
-            '1' { 
-                Write-Host 'Gathering summary info' -ForegroundColor Green
-                Start-Sleep -Seconds 1.5
-                Clear-Host
-                # An example of running an ad-hoc function in a separate process and ensuring no other commmands are supplied to possible elevated prompt
-                # NoExit flag allows main program to continue after calling
-                # '& {script block (note semicolons)}' is supplied to the Command flag of the ArgumentList
-                Start-Process powershell -ArgumentList "-NoExit", 
-                    "-Command", "& { 
-                        # Import the Get-SystemSummary function from the machine_summary.ps1 script
-                        . .\Scripts\Diagnostics\machine_summary.ps1; 
-                        # Execute the imported function to gather system summary information
-                        Get-SystemSummary;
-                        # Prompt the tech to press enter before closing the new window
-                        Read-Host 'Press enter to exit...';
-                        # Ensuring new PowerShell window is closed
-                        Exit
-                    }"
-             }
-            '2' {
-                Write-Host 'Triggered user simulation' -ForegroundColor Green
-                Start-Sleep -Seconds 1.5
-                Clear-Host
-                Invoke-SimulatorController
-             }
-            '3' {
-                Write-Host 'Triggered system logs' -ForegroundColor Green
-                Start-Sleep -Seconds 1.5
-                Clear-Host
-                Show-LogMenu
-             }
-            '4' {
-                # This can be made to run separate like above, just doing basic implementation (for now?)
-                Write-Host 'Querying users desktop usage' -ForegroundColor Green
-                Start-Sleep -Seconds 1.5
-                Clear-Host
+    $Selections = @()
 
-                $UserData = GetLastDesktopAccess
+    $Selections += [KeySelection]::new('1', "&[green]Gather summary info like PC name, IP, OS, etc.",
+                        {   Write-Host 'Gathering summary info' -ForegroundColor Green
+                            Start-Sleep -Seconds 1.5
+                            Clear-Host
+                            # An example of running an ad-hoc function in a separate process and ensuring no other commmands are supplied to possible elevated prompt
+                            # NoExit flag allows main program to continue after calling
+                            # '& {script block (note semicolons)}' is supplied to the Command flag of the ArgumentList
+                            Start-Process powershell -ArgumentList "-NoExit", 
+                                "-Command", "& { 
+                                    # Import the Get-SystemSummary function from the machine_summary.ps1 script
+                                    . .\Scripts\Diagnostics\machine_summary.ps1; 
+                                    # Execute the imported function to gather system summary information
+                                    Get-SystemSummary;
+                                    # Prompt the tech to press enter before closing the new window
+                                    Read-Host 'Press enter to exit...';
+                                    # Ensuring new PowerShell window is closed
+                                    Exit
+                                }" });
+                            
+    $Selections += [KeySelection]::new('2', "&[green]Run a user simulation on the system **Output in Results folder**",
+                        {   Write-Host 'Triggered user simulation' -ForegroundColor Green
+                            Start-Sleep -Seconds 1.5
+                            Clear-Host
+                            Invoke-SimulatorController });
 
-                Write-Host "Users:"
-                foreach ($User in $UserData) {
-                    Write-Host "`t$($User.UserName): $($User.LastAccess)"
-                }
+    $Selections += [KeySelection]::new('3', "&[green]Gather system logs. **Output in Results folder**",
+                        {   Write-Host 'Triggered system logs' -ForegroundColor Green
+                            Start-Sleep -Seconds 1.5
+                            Clear-Host
+                            Show-LogMenu });
 
-                Read-Host 'Press enter to continue...';
-                Clear-Host
-             }
-            '5' {
-                # Same as above, unsure on how to best implement for I.T usage
-                # May be a good candidate for the separate process workflow as seen for Summary Info
-                Write-Host 'Querying local drives for usage' -ForegroundColor Green
-                Start-Sleep -Seconds 1.5
-                Clear-Host
-                Write-Host "Please wait as drives are scanned... (May take a couple minutes)"
+    $Selections += [KeySelection]::new('4', "&[green]Query Users Last Desktop Usage",
+                        {   # This can be made to run separate like above, just doing basic implementation (for now?)
+                            Write-Host 'Querying users desktop usage' -ForegroundColor Green
+                            Start-Sleep -Seconds 1.5
+                            Clear-Host
 
-                # Unsure of I.T's policy or requirements, defaulting to 180 days
-                $DaysSince = 180
-                $LastDriveUsage = GetLastDriveUsage -SinceDate ([DateTime]::Now).AddDays(-($DaysSince))
-                
-                # Make it pretty :)
-                $TimeTaken = ($LastDriveUsage.TimeElapsed.TotalMilliseconds/1000).ToString("#.##")
-                
-                Write-Host "Finished scanning in $TimeTaken seconds"
+                            $UserData = GetLastDesktopAccess
 
-                if ($LastDriveUsage.LastUsage -eq $null) {
-                    Write-Host "No usage found in the last $DaysSince days!" -ForegroundColor Red
-                } else {
-                    $ModifiedFile = $LastDriveUsage.File
+                            Write-Host "Users:"
+                            foreach ($User in $UserData) {
+                                Write-Host "`t$($User.UserName): $($User.LastAccess)"
+                            }
 
-                    Write-Host "Found usage in the last $DaysSince days: " -ForegroundColor Green
-                    Write-Host "`tFile Modified: $($ModifiedFile.FullName)"
-                    Write-Host "`tModified At: $($ModifiedFile.LastWriteTime.ToShortDateString())"
-                }
+                            Read-Host 'Press enter to continue...';
+                            Clear-Host });
 
-                Read-Host 'Press enter to continue...';
-                Clear-Host
-             }
-            'q' {
-                Clear-Host
-                break
-             }
-            Default {
-                Write-Host 'Invalid option, please try again' -ForegroundColor Red
-                Start-Sleep -Seconds 1.5
-                Clear-Host
-            }
-        }
+    $Selections += [KeySelection]::new('5', "&[green]Check Drives for usage in the last 180 days",
+                        {   # Same as above, unsure on how to best implement for I.T usage
+                            # May be a good candidate for the separate process workflow as seen for Summary Info
+                            Write-Host 'Querying local drives for usage' -ForegroundColor Green
+                            Start-Sleep -Seconds 1.5
+                            Clear-Host
+                            Write-Host "Please wait as drives are scanned... (May take a couple minutes)"
 
-    } while (
-        $choice -ne 'q'
-    )    
+                            # Unsure of I.T's policy or requirements, defaulting to 180 days
+                            $DaysSince = 180
+                            $LastDriveUsage = GetLastDriveUsage -SinceDate ([DateTime]::Now).AddDays(-($DaysSince))
+                            
+                            # Make it pretty :)
+                            $TimeTaken = ($LastDriveUsage.TimeElapsed.TotalMilliseconds/1000).ToString("#.##")
+                            
+                            Write-Host "Finished scanning in $TimeTaken seconds"
+
+                            if ($LastDriveUsage.LastUsage -eq $null) {
+                                Write-Host "No usage found in the last $DaysSince days!" -ForegroundColor Red
+                            } else {
+                                $ModifiedFile = $LastDriveUsage.File
+
+                                Write-Host "Found usage in the last $DaysSince days: " -ForegroundColor Green
+                                Write-Host "`tFile Modified: $($ModifiedFile.FullName)"
+                                Write-Host "`tModified At: $($ModifiedFile.LastWriteTime.ToShortDateString())"
+                            }
+
+                            Read-Host 'Press enter to continue...';
+                            Clear-Host });
+
+    $PreviousMenuSelection = [KeySelection]::new('q', "&[yellow]Previous menu",
+                        {   Clear-Host });
+    $Selections += $PreviousMenuSelection
+
+    $Selection = $null
+    while ($Selection -eq $null -or $Selection.Key -ne $PreviousMenuSelection.Key) {
+        $Selection = QueryUserKeySelection -Question "&[yellow;darkgray] Please choose an option &[]`n" -Selections $Selections
+        $Selection.Run()
+    }
 }
