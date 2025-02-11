c7d8268d607802ea487f6fb88b00e983838c8fc0
Koupa <ktayl21@yahoo.com.au>
2025-02-12 09:29:01 +1000

    Create network_queries
    Created Powershell file containing Network machine query functionality

---
+++ FieldTechTool_Full/FieldTechTool_Lite/Scripts/Diagnostics/network_queries.ps1
@@ -0,0 +1,36 @@
+﻿function QueryNetworkMachine {
+    param($Server)
+
+    $Result = @{Online=$False; Users=@()}
+
+    $StoredEAP = $ErrorActionPreference
+    $ErrorActionPreference = "Stop"
+    try {
+        $Lines = @(quser /server:$Server) 
+        $Result.Online = $True
+
+        if ($Lines.Count -gt 1) {
+            $Headers = $Lines[0]
+            $DataPositions = @($Headers.IndexOf("USERNAME"), $Headers.IndexOf("SESSIONNAME"), $Headers.IndexOf("ID"), 
+                                $Headers.IndexOf("STATE"), $Headers.IndexOf("IDLE TIME"), $Headers.IndexOf("LOGON TIME"))
+
+            for ($i=1; $i -lt $Lines.Count; $i++) {
+                $UserData = $Lines[$i]
+                $Result.Users += @{ Username=$UserData.Substring($DataPositions[0], $DataPositions[1]-$DataPositions[0]).Trim();
+                                    SessionName=$UserData.Substring($DataPositions[1], $DataPositions[2]-$DataPositions[1]).Trim();
+                                    Id=$UserData.Substring($DataPositions[2], $DataPositions[3] - $DataPositions[2]).Trim();
+                                    State=$UserData.Substring($DataPositions[3], $DataPositions[4]-$DataPositions[3]).Trim();
+                                    IdleTime=$UserData.Substring($DataPositions[4], $DataPositions[5]-$DataPositions[4]).Trim();
+                                    LogonTime=$UserData.Substring($DataPositions[5], $UserData.Length - $DataPositions[5]).Trim() }
+            }
+        }
+
+
+    } catch {
+        #Write-Host "Server unavailable: $($_.Exception)"
+    } 
+
+    # Restore EAP
+    $ErrorActionPreference = $StoredEAP
+    return $Result
+}
