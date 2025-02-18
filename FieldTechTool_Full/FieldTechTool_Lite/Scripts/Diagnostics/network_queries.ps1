453674edb59a2807e7d2f8546fad69fd598f174b
Koupa <ktayl21@yahoo.com.au>
2025-02-18 10:18:22 +1000

    Rewrite Network Queries
    Added PingUtilities class, added StartAccessPointQuerying for local network testing, and rewrote some functionality & output

--- FieldTechTool_Full/FieldTechTool_Lite/Scripts/Diagnostics/network_queries.ps1
+++ FieldTechTool_Full/FieldTechTool_Lite/Scripts/Diagnostics/network_queries.ps1
@@ -1,4 +1,46 @@
-﻿function StartRemoteDesktopQuerying {
+﻿class PingUtilities {
+    static [String] ColorPing([int]$Ping) {
+        if ($Ping -le 100) { return "&[green]${Ping}ms&[]" } elseif ($Ping -le 250) { return "&[yellow]${Ping}ms&[]" } else { return "&[red]${Ping}ms&[]" }
+    }
+
+    static [String] ColorSuccessRate([pscustomobject] $PingStats) {
+        $Percentage = [double] ($PingStats.SuccessCount / $PingStats.TotalCount)
+        $Color = if ($Percentage -gt 0.9) { 'green' } elseif ($Percentage -gt 0.65) { 'yellow' } else { 'red' }
+        return $Percentage.ToString("&[$Color]0.#%")
+    }
+
+    static [String] ColorVariance([pscustomobject] $PingStats) {
+        $Color = if ($PingStats.VariancePercentage -le 0.15) { 'green' } elseif ($PingStats.VariancePercentage -le 0.35) { 'yellow' } else { 'red' }
+        return $PingStats.VariancePercentage.ToString("&[$Color]0.#%")
+    }
+
+    static [Hashtable] GetStatistics([pscustomobject[]]$Pings) {
+        $Result = @{}
+        
+        $SuccessfulPings = $Pings | Where-Object { $_.Success }
+
+        # Basic Min, Max, Avg
+        $Result.Average = [int] ($SuccessfulPings.ResponseTime | Measure-Object -Average).Average
+        $Result.Minimum = [int] ($SuccessfulPings.ResponseTime | Measure-Object -Minimum).Minimum
+        $Result.Maximum = [int] ($SuccessfulPings.ResponseTime | Measure-Object -Maximum).Maximum
+        
+        # This calculates the Ping variance in ms
+        #  Subtract the Average from each individual ping, square the result, add all results together and average them,
+        #   and finally, Square Root that average for the Std Dev
+        # To get this as a %, divide by Average Ping
+        $Result.Variance = [Math]::Sqrt((($SuccessfulPings | % { [Math]::Pow(($_.ResponseTime - $Result.Average), 2) }) | Measure-Object -Average).Average)
+        $Result.VariancePercentage = $Result.Variance / [Math]::Max(1, $Result.Average)
+
+        # Success counts
+        $Result.SuccessCount = $SuccessfulPings.Count
+        $Result.TotalCount = $Pings.Count
+        $Result.FailedCount = $Pings.Count - $SuccessfulPings.Count
+
+        return $Result
+    }
+}
+
+function StartRemoteDesktopQuerying {
     # Ask user what desktop/server
     $DesktopName = QueryUser -Question "&[Yellow]What Desktop/Server would you like to query?&[]`n>" -AnswerRequired
 
@@ -18,27 +60,7 @@
     # Ping Information
     #
 
-    # Method for formatting ping color
-    $FormatPing = {
-        param([int] $Ping)
-        if ($Ping -le 100) { return "&[green]${Ping}ms" } elseif ($Ping -le 250) { return "&[yellow]${Ping}ms" } else { return "&[red]${Ping}ms" }
-    }
-
-    # Method for formatting success rate %
-    $FormatSuccessRate = {
-        param([Hashtable]$PingCount)
-        $Percentage = [double] ($PingCount.Success / $PingCount.Total)
-        $Color = if ($Percentage -gt 0.9) { 'green' } elseif ($Percentage -gt 0.65) { 'yellow' } else { 'red' }
-        return $Percentage.ToString("&[$Color]0%")
-    }
-
-    $FormatPingVariance = {
-        param([double] $Variance, $Average)
-        $Variance = $Variance/$Average
-        $Color = if ($Variance -le 0.15) { 'green' } elseif ($Variance -le 0.35) { 'yellow' } else { 'red' }
-        return $Variance.ToString("&[$Color]0%")
-    }
-
+    
     $PingResults = $Result.Ping
     $PingStatus = if ($PingResults.Success) { "&[green]Successful" } else { "&[red]Failed" }
     
@@ -47,12 +69,14 @@
 
     # If Ping was successful, display ping stats
     if ($PingResults.Success) {
+        $PingStats = [PingUtilities]::GetStatistics($PingResults.Pings)
+
         $ResultsText = "Results: "
         $ResultOffset = "`t`t`t" # Fancy alignment
         [PowerIO]::DisplayText("`t`t&[gray]$ResultsText")
-        [PowerIO]::DisplayText("${ResultOffset}&[gray]Success Rate: $($FormatSuccessRate.Invoke($PingResults.Count))&[gray] [$($PingResults.Count.Success)/$($PingResults.Count.Total)]")
-        [PowerIO]::DisplayText("${ResultOffset}&[gray]Response Time: $($FormatPing.Invoke($PingResults.Average))&[gray] [Min: $($FormatPing.Invoke($PingResults.Minimum))&[gray], Max: $($FormatPing.Invoke($PingResults.Maximum))&[gray]]")
-        [PowerIO]::DisplayText("${ResultOffset}&[gray]Variance: $($FormatPingVariance.Invoke($PingResults.Variance, $PingResults.Average))&[gray] [+- $($PingResults.Variance.ToString('0.0'))ms]")
+        [PowerIO]::DisplayText("${ResultOffset}&[gray]Success Rate: $([PingUtilities]::ColorSuccessRate($PingStats))&[gray] [$($PingStats.SuccessCount)/$($PingStats.TotalCount)]")
+        [PowerIO]::DisplayText("${ResultOffset}&[gray]Response Time: $([PingUtilities]::ColorPing($PingStats.Average))&[gray] [Min: $([PingUtilities]::ColorPing($PingStats.Minimum))&[gray], Max: $([PingUtilities]::ColorPing($PingStats.Maximum))&[gray]]")
+        [PowerIO]::DisplayText("${ResultOffset}&[gray]Variance: $([PingUtilities]::ColorVariance($PingStats)))&[gray] [+- $($PingStats.Variance.ToString('0.#'))ms]")
     }
 
     #
@@ -84,23 +108,81 @@
     Write-Host "" # Newline separator
 }
 
-function QueryNetworkMachine {
-    param($Server, $PingTimeout=2000, $PingCount=5)
+function StartAccessPointQuerying {
+    # Filter down to Internet adapters, and only active ones
+    $NetworkConfigurations = Get-NetIPConfiguration | Where-Object { $_.IPv4DefaultGateway -ne $null -and $_.NetAdapter.Status -ne 'Disconnected' }
+    $ConfigDescriptors = $cfg | % { "&[yellow]$($_.InterfaceAlias) &[gray]($($_.InterfaceDescription))" }
 
-    $Result = @{Online=$False; Users=@(); Ping=@{Success=$False;Minimum=-1;Maximum=-1;Average=-1;Variance=-1;Count=@{Total=$PingCount;Failed=-1;Success=-1}}}
+    if ($NetworkConfigurations.Count -gt 1) {
+        $SelectedConfiguration = $NetworkConfigurations[(QueryUserSelection -Question "Which Network Adapter are you using?" -Answers $ConfigDescriptors)]
+    } elseif ($NetworkConfigurations.Count -eq 1) {
+        $SelectedConfiguration = $NetworkConfigurations[0]
+    } else {
+        throw "No NetworkConfigurations to test!"
+    }
 
-    # Before we do anything, start the ping job in the background
-    $PingJob = Start-Job -ScriptBlock {
+    # Look, I haven't actually found a way to specify the SOURCE Interface/Address
+    # Atleast, not with the way I do pings (Superior method anyways lol)
+    # But by pinging the gateway, we can only reach it from the desired (or connected) adapter anyway!
+    $DefaultGateway = $SelectedConfiguration.IPv4DefaultGateway[0].NextHop
+
+    [PowerIO]::DisplayText("`n&[gray]Testing '$($SelectedConfiguration.InterfaceAlias)', Pinging IPv4 Gateway '$DefaultGateway'")
+    
+    $PingResults = PingServer -Server $DefaultGateway -Count 25
+    $PingStatus = if ($PingResults.Success) { "&[green]Successful" } else { "&[red]Failed" }
+    
+    # Display the Ping Status
+    # Copypasta from the other method
+    [PowerIO]::DisplayText("`t&[gray]Ping: $PingStatus")
+    if ($PingResults.Success) { # If any succeed
+        $PingStats = [PingUtilities]::GetStatistics($PingResults)
+
+        $ResultsText = "Results: "
+        $ResultOffset = "`t`t`t" # Fancy alignment
+        [PowerIO]::DisplayText("`t`t&[gray]$ResultsText")
+        [PowerIO]::DisplayText("${ResultOffset}&[gray]Success Rate: $([PingUtilities]::ColorSuccessRate($PingStats))&[gray] [$($PingStats.SuccessCount)/$($PingStats.TotalCount)]")
+        [PowerIO]::DisplayText("${ResultOffset}&[gray]Response Time: $([PingUtilities]::ColorPing($PingStats.Average))&[gray] [Min: $([PingUtilities]::ColorPing($PingStats.Minimum))&[gray], Max: $([PingUtilities]::ColorPing($PingStats.Maximum))&[gray]]")
+        [PowerIO]::DisplayText("${ResultOffset}&[gray]Variance: $([PingUtilities]::ColorVariance($PingStats))&[gray] [+- $($PingStats.Variance.ToString('0.#'))ms]")
+    }
+
+    Write-Host "" # Newline separator
+}
+
+function PingServer {
+    param($Server, $Timeout=2000, $Count=10, [switch]$AsJob, [ScriptBlock]$PingEventHandler)
+
+    $PingScript = {
         param($Server, $Timeout, $Count)
         $Pings = @() # List of pings
         for ($i = 0; $i -lt $Count; $i++) {
             $Ping = (Get-WmiObject -Class Win32_PingStatus -Filter ('Address="{0}" and Timeout={1}' -f $Server, $Timeout) | Select-Object ResponseTime, StatusCode)
-            $Pings += @{Success=($Ping.StatusCode -eq 0);
-                        ResponseTime=($Ping.ResponseTime);}
+            $PingResult = @{Success=($Ping.StatusCode -eq 0);
+                            StatusCode=($Ping.StatusCode);
+                            ResponseTime=($Ping.ResponseTime);}
+            $Pings += $PingResult
+
+            if ($PingEventHandler -ne $null) {
+                $PingEventHandler.Invoke($PingResult)
+            }
         }
         return $Pings
-    } -ArgumentList $Server, $PingTimeout, $PingCount
+    }
 
+    if (-not $AsJob) {
+        return $PingScript.Invoke($Server, $Timeout, $Count)
+    }
+
+    $PingJob = Start-Job -ScriptBlock $PingScript -ArgumentList $Server, $Timeout, $Count
+    return $PingJob
+}
+
+function QueryNetworkMachine {
+    param($Server, $PingTimeout=2000, $PingCount=5)
+
+    $Result = @{Online=$False; Users=@(); Ping=@{Success=$False;Pings=@()}}
+
+    # Before we do anything, start the ping job in the background
+    $PingJob = PingServer -Server $Server -Timeout $PingTimeout -Count $PingCount -AsJob
 
     # Set our EAP so that the 'quser' function actually throws an exception
     $StoredEAP = $ErrorActionPreference
@@ -135,26 +217,17 @@ function QueryNetworkMachine {
         $ErrorActionPreference = $StoredEAP
     }
 
-
-
-
     # Unsure of all the states right now, looking for number states rather than Str Compare
     if ($PingJob.State -eq "Completed") {
         $PingResults = Receive-Job $PingJob -Wait -AutoRemoveJob
         
         # If all of our pings failed, then pinging failed!
-        $SuccessfulPings = ($PingResults | Where-Object { $_.Success })
-        $Failed = $SuccessfulPings.Count -eq 0
+        $Failed = ($PingResults | Where-Object { $_.Success }).Count -eq 0
 
-        $Result.Ping.Count.Success = $SuccessfulPings.Count 
-        $Result.Ping.Count.Failed = $PingCount - $SuccessfulPings.Count
-
+        # Mark Pinging as a success if it wasn't a complete fail!
         if (-not $Failed) {
-            $Result.Ping.Success = $True
-            $Result.Ping.Average = [int] ($SuccessfulPings.ResponseTime | Measure-Object -Average).Average
-            $Result.Ping.Minimum = [int] ($SuccessfulPings.ResponseTime | Measure-Object -Minimum).Minimum
-            $Result.Ping.Maximum = [int] ($SuccessfulPings.ResponseTime | Measure-Object -Maximum).Maximum
-            $Result.Ping.Variance = $Result.Ping.Average - $Result.Ping.Minimum
+            $Result.Ping.Success = $True    
+            $Result.Ping.Pings = $PingResults
         }
     }
 
